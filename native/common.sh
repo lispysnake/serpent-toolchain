a69d8670b78e50dbf018f6bdcc4a225ad09ca168
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-05-24 18:55:57 +0100

    native: Attempt rebootstrap and deploy
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- native/common.sh
+++ native/common.sh
@@ -7,6 +7,7 @@ export SERPENT_BUILD_DIR="${TOP_DIR}/build_native"
 export HOME="${SERPENT_BUILD_DIR}"
 export SERPENT_ROOT_DIR="${TOP_DIR}"
 export SERPENT_BUILD_JOBS=$(nproc)
+export SERPENT_DEPLOY_DIR="${TOP_DIR}/deploy_native"
 
 unset SCRIPT_PATH
 unset TOP_DIR
--- native/ldc.sh
+++ native/ldc.sh
@@ -4,14 +4,14 @@ set -e
 . $(dirname $(realpath -s $0))/common.sh
 
 # Set up environment to build LDC
-LDC_BUILD_DIR="${SERPENT_BUILD_DIR}/ldc"
 LDC_ROOT_DIR="${SERPENT_ROOT_DIR}/external/ldc"
 LDC_BOOTSTRAP_DIR="${SERPENT_BUILD_DIR}/ldc-bootstrap"
+LDC_NATIVE_DIR="${SERPENT_BUILD_DIR}/ldc-bootstrap"
 
 # Stage the root build directory
-rm -rf "${LDC_BUILD_DIR}"
+rm -rf "${LDC_NATIVE_DIR}"
 rm -rf "${LDC_BOOTSTRAP_DIR}"
-install -D -d -m 00755 "${LDC_BUILD_DIR}"
+install -D -d -m 00755 "${LDC_NATIVE_DIR}"
 install -D -d -m 00755 "${LDC_BOOTSTRAP_DIR}"
 
 clean_external "ldc"
@@ -23,7 +23,24 @@ cmake -GNinja "${LDC_ROOT_DIR}" \
     -DBUILD_SHARED_LIBS=OFF \
     -DD_COMPILER_ID="ldc2" \
     -DCMAKE_INSTALL_PREFIX="${LDC_BOOTSTRAP_DIR}/install" \
-    -DLDC_DYNAMIC_COMPILE=OFF
+    -DLDC_DYNAMIC_COMPILE=OFF \
+    -DLDC_WITH_LLD=OFF
 
 ninja -j${SERPENT_BUILD_JOBS}
 ninja -j${SERPENT_BUILD_JOBS} install
+popd
+
+clean_external "ldc"
+echo "Building LDC pure"
+pushd "${LDC_NATIVE_DIR}"
+cmake -GNinja "${LDC_ROOT_DIR}" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=BOTH \
+    -DD_COMPILER="${LDC_BOOTSTRAP_DIR}/install/bin/ldc2" \
+    -DCMAKE_INSTALL_PREFIX="${SERPENT_DEPLOY_DIR}" \
+    -DLDC_DYNAMIC_COMPILE=ON \
+    -DLDC_WITH_LLD=OFF
+
+ninja -j${SERPENT_BUILD_JOBS}
+ninja -j${SERPENT_BUILD_JOBS} install
+popd
