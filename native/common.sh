9d34e5a044cf4da63eca76af9a04f2318afd01f5
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-05-24 18:28:27 +0100

    common: Add proper CFLAGS + ccache integration
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- native/common.sh
+++ native/common.sh
@@ -6,9 +6,23 @@ TOP_DIR=`dirname "${SCRIPT_PATH}"`
 export SERPENT_BUILD_DIR="${TOP_DIR}/build_native"
 export HOME="${SERPENT_BUILD_DIR}"
 export SERPENT_ROOT_DIR="${TOP_DIR}"
+export SERPENT_BUILD_JOBS=$(nproc)
 
 unset SCRIPT_PATH
 unset TOP_DIR
+
+export CC="gcc"
+export CXX="g++"
+
+# ccache breaks ldmd, so manually force it
+if [[ -e "/usr/lib64/ccache/bin" ]]; then
+    export PATH="/usr/lib64/ccache/bin:$PATH"
+elif [[ -e "/usr/lib/ccache/bin" ]]; then
+    export PATH="/usr/lib/ccache/bin:$PATH"
+fi
+
+export CFLAGS="-mtune=generic -march=x86-64 -g2 -O2 -pipe -fPIC -D_FORTIFY_SOURCE=2 -fstack-protector-strong -Wformat -Wall -Wno-error -Wp,-D_REENTRANT"
+export CXXFLAGS="-mtune=generic -march=x86-64 -g2 -O2 -pipe -fPIC -D_FORTIFY_SOURCE=2 -fstack-protector-strong -Wall -Wno-error -Wp,-D_REENTRANT"
 
 # Purge and restage the external directory
 function clean_external()
