f4e95e1bc17e84c1ed30702e194f60e0e8b172a6
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-05-25 00:09:50 +0100

    redmond/common: Add helper to fetch tarballs for us
    This also verifies the sha256sum to keep on the safe side.
    
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- redmond/common.sh
+++ redmond/common.sh
@@ -8,9 +8,36 @@ export HOME="${SERPENT_BUILD_DIR}"
 export SERPENT_ROOT_DIR="${TOP_DIR}"
 export SERPENT_BUILD_JOBS=$(nproc)
 export SERPENT_DEPLOY_DIR="${TOP_DIR}/deploy_redmond"
+export SERPENT_DOWNLOAD_DIR="${TOP_DIR}/downloads"
 
 unset SCRIPT_PATH
 unset TOP_DIR
 
 export CC="gcc"
 export CXX="g++"
+
+function fetch_tarball()
+{
+    local pkgFile="${SERPENT_ROOT_DIR}/external/${1}"
+    local url="$(cat ${pkgFile} | cut -d ' ' -f 1)"
+    local sha="$(cat ${pkgFile} | cut -d ' ' -f 2)"
+    local tarball=$(basename "${url}")
+
+    if [[ ! -d "${SERPENT_DOWNLOAD_DIR}" ]]; then
+        install -d -D -m 00755 "${SERPENT_DOWNLOAD_DIR}"
+    fi
+    local tarballTarget="${SERPENT_DOWNLOAD_DIR}/${tarball}"
+
+    if [[ ! -e "${tarballTarget}" ]]; then
+        echo "Downloading: ${tarball}"
+        curl --output "${tarballTarget}" "${url}"
+    fi
+
+    echo "Computing SHA256SUM: ${tarball}"
+    local dloadSha=$(sha256sum "${tarballTarget}" | cut -d ' ' -f 1)
+    if [[ "${dloadSha}" != "${sha}" ]]; then
+        echo "Invalid SHA256sum for ${tarballTarget}"
+        exit 1
+    fi
+    echo "SHA256SUM verification passed."
+}
