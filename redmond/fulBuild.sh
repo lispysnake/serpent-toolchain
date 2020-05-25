8926687516d6e97a1468eb3f624cf56edfb0bfe0
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-05-26 00:13:19 +0100

    redmond/fullBuild: Account for all stages now
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- redmond/fulBuild.sh
+++ redmond/fulBuild.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
+echo "Preparing initial toolchain"
+
 $(dirname $(realpath -s $0))/binutils.sh
 $(dirname $(realpath -s $0))/headers.sh
 $(dirname $(realpath -s $0))/gcc-stage1.sh
 $(dirname $(realpath -s $0))/crt.sh
 $(dirname $(realpath -s $0))/gcc-stage2.sh
 $(dirname $(realpath -s $0))/mingw64.sh
+
+echo "Preparing tools"
+$(dirname $(realpath -s $0))/pkgconf.sh
+
+
+echo "Preparing runtime support"
+$(dirname $(realpath -s $0))/zlib.sh
+$(dirname $(realpath -s $0))/libpng.sh
+
+echo "Preparing SDL"
+$(dirname $(realpath -s $0))/sdl.sh
+$(dirname $(realpath -s $0))/sdl_image.sh
