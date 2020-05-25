5e77f5b5222a79115fc5b621402a5566b823d3c2
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-05-25 20:41:13 +0100

    redmond: Add libpng build script
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

---
+++ redmond/libpng.sh
@@ -0,0 +1,28 @@
+
+#!/usr/bin/env -S -i bash --norc --noprofile
+set -e
+
+. $(dirname $(realpath -s $0))/common.sh
+
+# Set up environment to build SDL
+LIBPNG_ROOT_DIR="${SERPENT_ROOT_DIR}/external/libpng"
+LIBPNG_BUILD_DIR="${SERPENT_BUILD_DIR}/libpng"
+
+# Stage the root build directory
+rm -rf "${LIBPNG_BUILD_DIR}"
+install -D -d -m 00755 "${LIBPNG_BUILD_DIR}"
+
+clean_external "libpng"
+
+echo "Building libpng"
+
+pushd "${LIBPNG_BUILD_DIR}"
+"${LIBPNG_ROOT_DIR}/configure" \
+    --prefix=/ \
+    --with-sysroot="${SERPENT_DEPLOY_DIR}" \
+    --host=x86_64-w64-mingw32 \
+    --enable-shared \
+    --enable-static \
+
+make -j${SERPENT_BUILD_JOBS}
+make -j${SERPENT_BUILD_JOBS} install DESTDIR="${SERPENT_DEPLOY_DIR}"
