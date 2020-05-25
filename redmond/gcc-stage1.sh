011439b92f83aabffc2a9c881ab2ee0f9d502ffd
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-05-25 13:18:24 +0100

    redmond: Add gcc-stage1 build script
    We build the core GCC compiler for multilib, statically linked, so that
    we can build the crt and eventually gcc-stage2.
    
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

---
+++ redmond/gcc-stage1.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env -S -i bash --norc --noprofile
+set -e
+
+. $(dirname $(realpath -s $0))/common.sh
+
+export GCC_ROOT_DIR="${SERPENT_STAGING_DIR}/gcc"
+export GCC_BUILD_DIR="${SERPENT_BUILD_DIR}/gcc-stage1"
+
+# Stage the root build directory
+rm -rf "${GCC_BUILD_DIR}"
+install -D -d -m 00755 "${GCC_BUILD_DIR}"
+
+fetch_tarball gcc
+fetch_tarball mpfr
+fetch_tarball gmp
+fetch_tarball mpc
+
+extract_tarball gcc
+extract_tarball mpfr
+extract_tarball gmp
+extract_tarball mpc
+
+echo "Building gcc-stage1"
+pushd "${GCC_BUILD_DIR}"
+
+ln -s "${SERPENT_STAGING_DIR}/mpfr" .
+ln -s "${SERPENT_STAGING_DIR}/mpc" .
+ln -s "${SERPENT_STAGING_DIR}/gmp" .
+
+"${GCC_ROOT_DIR}/configure" \
+    --prefix=/ \
+    --with-sysroot="${SERPENT_DEPLOY_DIR}" \
+    --target=x86_64-w64-mingw32 \
+    --enable-targets=all \
+    --disable-nls \
+    --with-multilib-list=m32,m64 \
+    --enable-multilib \
+    --disable-shared \
+    --enable-static \
+    --enable-languages=c,c++
+
+make -j${SERPENT_BUILD_JOBS} all-gcc
+
+echo "Installing gcc-stage1"
+make -j${SERPENT_BUILD_JOBS} install-gcc DESTDIR="${SERPENT_DEPLOY_DIR}"
